fd7888fe66a4f627bbd8a83d1412b4d9ee783db5
Victor <remmark999@gmail.com>
2023-08-28 17:03:19 +0300

    Repo init

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    extends: ["airbnb", "airbnb-typescript", "prettier"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint", "prettier"],
+    root: true,
+    parserOptions: {
+        project: "./tsconfig.json",
+    },
+    rules: {
+        "prettier/prettier": [
+            "error",
+            {
+                tabWidth: 4,
+            },
+        ],
+        // Since this is the only easy way of io on node
+        "no-console": "off",
+    },
+};
